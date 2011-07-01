a975620e5afb121bd51376ec3b279d205cff0868
halvors <halvors@skymiastudios.com>
2011-07-01 15:54:57 +0200

    Added RandomName class.

---
+++ src/main/java/com/halvors/lupi/wolf/RandomName.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2011 halvors <halvors@skymiastudios.com>
+ * Copyright (C) 2011 speeddemon92 <speeddemon92@gmail.com>
+ *
+ * This file is part of Lupi.
+ *
+ * Lupi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lupi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Lupi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.halvors.lupi.wolf;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomName {
+	private final static List<String> names = new ArrayList<String>();
+	
+	static {
+		initRandomNames();
+	}
+	
+    /**
+     * Generate the table of premade wolf names.
+     */
+    private static void initRandomNames() {  
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(WolfManager.class.getResourceAsStream("wolfNames.txt")));
+           
+            while (true) {
+                String s1;
+                
+                if ((s1 = bufferedReader.readLine()) == null) {
+                    break;
+                }
+                
+                s1 = s1.trim();
+                
+                if (s1.length() > 0) {
+                    names.add(s1);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Generate a random name.
+     * 
+     * @return String
+     */
+    public static String getRandomName() {
+        Random random = new Random();
+        
+        return names.get(random.nextInt(names.size() - 1 ));
+    }
+}
