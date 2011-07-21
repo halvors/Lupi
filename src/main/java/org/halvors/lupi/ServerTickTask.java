f44eba7f4b807ad18e9f418446eede2e066e46bb
halvors <halvors@skymiastudios.com>
2011-07-21 19:55:47 +0200

    Cleanup up the tick based entity check.

--- src/main/java/org/halvors/lupi/ServerTickTask.java
+++ src/main/java/org/halvors/lupi/ServerTickTask.java
@@ -1,43 +1,10 @@
 package org.halvors.lupi;
 
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
-import org.halvors.lupi.wolf.Wolf;
-import org.halvors.lupi.wolf.WolfManager;
-import org.halvors.lupi.wolf.inventory.WolfInventory;
+import org.halvors.lupi.util.WolfUtil;
 
 public class ServerTickTask implements Runnable {
-	private final WolfManager wolfManager = WolfManager.getInstance();
-	
 	@Override
 	public void run() {
-		checkNearbyEntities();
-	}
-	
-	private void checkNearbyEntities() {
-		for (World world : Bukkit.getServer().getWorlds()) {
-			for (Entity entity : world.getEntities()) {
-				if (entity instanceof org.bukkit.entity.Wolf) {
-					org.bukkit.entity.Wolf bukkitWolf = (org.bukkit.entity.Wolf) entity;
-					
-					if (bukkitWolf.isTamed() && wolfManager.hasWolf(bukkitWolf)) {
-						Wolf wolf = wolfManager.getWolf(bukkitWolf);
-						
-						for (Entity entityItem : bukkitWolf.getNearbyEntities(1, 1, 1)) {
-							// Make wolf pickup item, remove the dropped item and add it to wolf's inventory.
-							if (entityItem instanceof Item) {
-								WolfInventory wi = wolf.getInventory();
-								Item item = (Item) entityItem;
-								
-								wi.addItem(item.getItemStack());
-								item.remove();
-							}
-						}
-					}
-				}
-			}
-		}
+		WolfUtil.doNearbyEntityCheck();
 	}
 }
--- src/main/java/org/halvors/lupi/util/WolfUtil.java
+++ src/main/java/org/halvors/lupi/util/WolfUtil.java
@@ -7,9 +7,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.halvors.lupi.wolf.Wolf;
+import org.halvors.lupi.wolf.WolfManager;
+import org.halvors.lupi.wolf.inventory.WolfInventory;
 
 public class WolfUtil {
+	private static WolfManager wolfManager = WolfManager.getInstance();
+	
 	/**
 	 * Get Entity by uniqueId.
 	 * 
@@ -62,6 +67,27 @@ public class WolfUtil {
 	}
 	
 	public static void doNearbyEntityCheck() {
-		
+		for (World world : Bukkit.getServer().getWorlds()) {
+			for (Entity entity : world.getEntities()) {
+				if (entity instanceof org.bukkit.entity.Wolf) {
+					org.bukkit.entity.Wolf bukkitWolf = (org.bukkit.entity.Wolf) entity;
+						
+					if (bukkitWolf.isTamed() && wolfManager.hasWolf(bukkitWolf)) {
+						Wolf wolf = wolfManager.getWolf(bukkitWolf);
+							
+						for (Entity entityItem : bukkitWolf.getNearbyEntities(1, 1, 1)) {
+							// Make wolf pickup item, remove the dropped item and add it to wolf's inventory.
+							if (entityItem instanceof Item) {
+								WolfInventory wi = wolf.getInventory();
+								Item item = (Item) entityItem;
+									
+								wi.addItem(item.getItemStack());
+								item.remove();
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
