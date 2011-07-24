9aaecd4a399ec87c68529b7fe9098bfea0107cc4
halvors <halvors@skymiastudios.com>
2011-07-24 07:30:45 +0200

    Added missing file.

---
+++ src/main/java/org/halvors/lupi/listener/LupiListener.java
@@ -0,0 +1,19 @@
+package org.halvors.lupi.listener;
+
+import org.halvors.lupi.Lupi;
+import org.halvors.lupi.event.LupiServerTickEvent;
+import org.halvors.lupi.util.WolfUtil;
+
+public class LupiListener extends org.halvors.lupi.event.LupiListener {
+//	private final Lupi plugin;
+	
+	public LupiListener(Lupi plugin) {
+//		this.plugin = plugin;
+	}
+	
+	@Override
+	public void onLupiServerTick(LupiServerTickEvent event) {
+		// Do the nearby entity check.
+		WolfUtil.doNearbyEntityCheck();
+	}
+}
